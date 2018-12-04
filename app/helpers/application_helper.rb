28509c28dad14d74811821b38ac0d806c29cb915
swol1 <den.gavrilin1@gmail.com>
2018-12-04 23:12:24 +0300

    Added sklonyator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+    ostatok = number % 10
+    ostatok_big = number % 100
+
+    if ostatok_big.between?(10,14) || ostatok.between?(5,9) || ostatok == 0
+      krokodilov
+    elsif ostatok == 1
+      krokodil
+    else
+      krokodila
+    end
+  end
 end
