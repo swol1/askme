a7232afa68e5f05ec834624fb84353081f9e7747
swol1 <den.gavrilin1@gmail.com>
2018-12-08 12:57:22 +0300

    Added user logged in after registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user.present?
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'вы успешно залогинились'
+      redirect_to root_url, notice: 'Вы успешно залогинились'
     else
       flash.now.alert = 'Неправильный имейл или пароль'
       render :new
