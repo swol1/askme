686b52df84d5cb2e14e46cca6ef20fd849d274cf
swol1 <den.gavrilin1@gmail.com>
2018-12-14 19:22:07 +0300

    Allow only specific colors for background selection

--- db/migrate/20181213155646_add_background_to_user.rb
+++ db/migrate/20181213155646_add_background_to_user.rb
@@ -1,5 +1,5 @@
 class AddBackgroundToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user_background, :string
+    add_column :users, :user_background, :string, :default => "#2e2f41"
   end
 end
